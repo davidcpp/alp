78937a2e748745c15fa6e0fce7a6a098787f7c02
Bartek Solarski <bartsol21@gmail.com>
2020-05-16 20:41:13 +0200

    wgrany layout + obsluga wynikow z roznych lig

--- alp/urls.py
+++ alp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.match_list, name='match_list'),
+    path('', views.league_list, name='league_list'),
     path('matches/<str:league_name>/', views.league_match_list, name='league_match_list'),
+
 ]
--- alp/views.py
+++ alp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_list_or_404, render
 from .models import Match, League
 
-def match_list(request):
-    matches = Match.objects.all()
-    return render(request, 'alp/match_list2.html', {'matches' : matches})
-
 def league_match_list(request, league_name):
     matches = get_list_or_404(Match, league__league_name = league_name)
-    return render(request, 'alp/matches_results.html', {'matches' : matches, 'league_name': league_name})
+    leagues = get_list_or_404(League)
+    return render(request, 'alp/matches_results.html', {'matches' : matches, 'league_name': league_name, 'leagues': leagues})
+
+def league_list(request):
+    leagues = get_list_or_404(League)
+    return render(request, 'alp/home.html', {'leagues': leagues})
