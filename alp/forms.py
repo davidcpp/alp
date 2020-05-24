74aae076001d2f73e0c27068053b3e1a914514bb
Bartek Solarski <bartsol21@gmail.com>
2020-05-24 12:15:17 +0200

    dodanie daty do formularza poprawka labela w formularza do dodawania druzyny

--- alp/forms.py
+++ alp/forms.py
@@ -11,6 +11,7 @@ class Note_Form(forms.ModelForm):
             'note': 'Treść',
         }
 
+
 class League_Form(forms.ModelForm):
     class Meta:
         model = League
@@ -27,7 +28,7 @@ class Team_Form(forms.ModelForm):
         labels = {
             'league': 'Nazwa ligi',
             'team_name': 'Nazwa drużyny',
-            'team_short': 'Skrócona nazwa drużyny',
+            'team_short': 'Nazwa skrócona',
             'place': 'Miejscowość',
             'comments': 'Uwagi',
         }
@@ -36,7 +37,7 @@ class Match_Form(forms.ModelForm):
     class Meta:
         model = Match
         fields = ('league', 'host_team', 'guest_team', 'host_team_goals', 'guest_team_goals', 'round_game',
-                  'place', 'comments')
+                  'place', 'comments', 'game_date')
         labels = {
             'league': 'Nazwa ligi',
             'host_team': 'Gospodarz',
@@ -46,4 +47,5 @@ class Match_Form(forms.ModelForm):
             'round_game': 'Kolejka',
             'place': 'Miejscowość',
             'comments': 'Komentarz',
+            'game_date': 'Data',
         }
