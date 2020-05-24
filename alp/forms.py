836da863e832c9be4b0d800df3b3b13e65946f00
Adam Jakubowski <adamjakubowski@postgraduate.put.poznan.pl>
2020-05-24 14:30:07 +0200

    Kolejne poprawki w walidacji formularza

--- alp/forms.py
+++ alp/forms.py
@@ -41,20 +41,18 @@ class Match_Form(forms.ModelForm):
         host_team_goals = self.cleaned_data['host_team_goals']
         round_game = self.cleaned_data['round_game']
         league = self.cleaned_data['league']
-        if host_team == guest_team:
+        if str(host_team) == str(guest_team):
             self._errors['host_team'] = self.error_class(['Jako drużynę gospodarzy i gości wybrano tą samą drużynę'])
         if guest_team_goals > 150:
             self._errors['guest_team_goals'] = self.error_class(['Zbyt duża liczba bramek strzelonych przez drużynę gości, maksymalna wartość 150'])
         if host_team_goals > 150:
             self._errors['host_team_goals'] = self.error_class(['Zbyt duża liczba bramek strzelonych przez drużynę gospodarzy, maksymalna wartość 150'])
         if round_game < 1:
-            self._errors['host_team_goals'] = self.error_class(['Błędny nr kolejki'])
+            self._errors['round_game'] = self.error_class(['Błędny nr kolejki'])
         teams = Team.objects.all().filter(league__league_name=str(league))
         league_teams_name = [team.team_name for team in teams]
-        print(league_teams_name)
-        print(host_team)
         if str(host_team) not in league_teams_name:
-            self._errors['host_team'] = self.error_class(['Drużyna gospodarzy nie jest przypisana do wybranej ligi'])
+            self._errors['host_team'] += self.error_class(['Drużyna gospodarzy nie jest przypisana do wybranej ligi'])
         if str(guest_team) not in league_teams_name:
             self._errors['guest_team'] = self.error_class(['Drużyna gości nie jest przypisana do wybranej ligi'])
         return self.cleaned_data
